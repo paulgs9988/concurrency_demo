7cfc819fa34e6405c335d33a90150a9d671911c2
Paul Sullivan <paul@startslips.com>
2022-10-03 13:58:57 -0400

    first commit

---
+++ code/main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"math/rand"
+	"time"
+)
+
+var cache = map[int]Book{}
+var rnd = rand.New(rand.NewSource(time.Now().UnixNano()))
+
+func main() {
+	for i := 0; i < 10; i++ {
+		id := rnd.Intn(10) + 1
+	}
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module www.github.com/paulgs9988/concurrencydemo
+
+go 1.18
